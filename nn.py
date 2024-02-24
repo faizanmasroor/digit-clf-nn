487c5cbd407082e7a048f8fb0c8c956025d4cea2
Faizan Masroor (Post Oak) <faizanmasroor01@gmail.com>
2024-02-23 22:32:54 -0600

    Added pytorch installation comments.

---
+++ nn.py
@@ -0,0 +1,8 @@
+"""
+if using Harvey GPU, run the following command to install pytorch:
+    "conda install pytorch torchvision torchaudio pytorch-cuda=12.1 -c pytorch -c nvidia"
+otherwise:
+    "conda install pytorch torchvision torchaudio cpuonly -c pytorch"
+"""
+
+import torch
